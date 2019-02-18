a019152ce879f82bd2b144bfd8c5b9a1825e6b11
joakim lundberg <joakim.lundberg123@gmail.com>
2019-02-18 14:37:49 +0100

    .

--- memo/LevelBuilderViewController.swift
+++ memo/LevelBuilderViewController.swift
@@ -241,8 +241,6 @@ class LevelBuilderViewController: UIViewController {
                 alert.addAction(UIAlertAction(title: "try again", style: .default, handler: nil))
                 self.present(alert, animated: true)
                 
-                
-                
                 antLives = 3
                 levelCount = 1
                 points = 0
